cadcf815a933dd460d68a1338826c1a4a41d4b4e
= <adamjcsmith@gmail.com>
2016-04-11 15:59:43 +0100

    Fixed ES6 issue with buildscript

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
-gulp.task('default', () => {
+gulp.task('default', function() {
 	return gulp.src('static/src_images/*')
 		.pipe(imagemin({
 			progressive: true,
