4cf6ad781ed638ce1598478a3bb35615262b4395
= <adamjcsmith@gmail.com>
2016-04-12 12:03:46 +0100

    Added sass/scss and more Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,38 @@
 var gulp = require('gulp');
+
+/* Image */
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
-gulp.task('default', function() {
-	return gulp.src('static/src_images/*')
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [pngquant()]
-		}))
-		.pipe(gulp.dest('static/images'));
+/* CSS */
+var sass = require('gulp-sass');
+var sassdoc = require('sassdoc');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var sassOptions = { errLogToConsole: true, outputStyle: 'compressed' };
+var sassInput = 'static/sass/*.scss';
+var sassOutput = 'static/css';
+
+gulp.task('sass', function() {
+  return gulp.src(sassInput)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(sassOutput))
+    .pipe(sassdoc())
+    .resume();
 });
+
+gulp.task('imagemin', function() {
+  return gulp
+    .src('static/src_images/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('static/images'));
+});
+
+gulp.task('default', ['sass', 'imagemin']);
