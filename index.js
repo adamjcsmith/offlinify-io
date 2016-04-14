5fafab6e7abb68866ed87b54df81465e80ead1b0
= <adamjcsmith@gmail.com>
2016-04-14 10:10:38 +0100

    Added API

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var compression = require('compression');
+var cors = require('cors');
 
 var _ = require('lodash');
 var jade = require('jade');
+
+var jsonData = require('./data/offlinify-data.json');
+var jsonDataBig = require('./data/offlinify-data-big.json');
 
 // view setup
 app.set('views', './views');
@@ -19,6 +23,31 @@ app.use('/static', express.static('static'));
 // routes
 app.get('/', function(req, res) {
 	res.render('index', {title: 'This is a title', message: 'This is text'});
+});
+
+// api
+app.get('/get', function(req, res) {
+	var filteredData = jsonData;
+	if(req.query.after) {
+      filteredData = _.filter(filteredData, function(o) {
+        return Date.parse(o.timestamp) > Date.parse(req.query.after);
+      });
+    }
+	res.json(filteredData);
+});
+
+app.get('/getBig', function(req, res) {
+	var filteredData = jsonDataBig;
+	if(req.query.after) {
+      filteredData = _.filter(filteredData, function(o) {
+        return Date.parse(o.timestamp) > Date.parse(req.query.after);
+      });
+    }
+	res.json(filteredData);
+});
+
+app.post('/post', function(req, res) {
+	console.log("Post request received.");
 });
 
 // console
